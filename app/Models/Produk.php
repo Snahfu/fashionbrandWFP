f03dc6469054d0ba33d75b3199b4a6fb277fa6ec
reynardnathanael <jiangreynard@gmail.com>
2023-07-06 10:55:04 +0700

    sweetalert

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Produk extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function jenis()
     {
         return $this->belongsTo(Jenis::class);
+    }
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class);
     }
 
     public function orders()
