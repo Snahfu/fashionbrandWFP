9a982dbdadc6afa82551dae901e84e9b9fa70fbb
Snahfu <hanxin131@gmail.com>
2023-07-08 11:50:48 +0700

    update user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Jenis;
 use App\Models\Kategori;
 use App\Models\Produk;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(KategoriSeeder::class, false, ["path" => "database/data/1-kategoris.csv", "model" => Kategori::class]);
         $this->call(JenisSeeder::class, false, ["path" => "database/data/2-jenises.csv", "model" => Jenis::class]);
         $this->call(ProdukSeeder::class, false, ["path" => "database/data/3-produks.csv", "model" => Produk::class]);
+        $this->call(UserSeeder::class, false, ["path" => "database/data/4-users.csv", "model" => User::class]);
     }
 }
